95348dc3ab3f9443253c32b6a40ce69dcae03173
mabel-chang <mabelchang00@gmail.com>
2023-04-12 18:50:12 -0400

    changes to js script

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -34,23 +34,55 @@ function cutRestaurantList(list) {
   }));
 }
 
+function initMap(){
+  const carto = L.map('map').setView([38.98, -76.93], 13); //long, lat
+
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(carto);
+  return carto;
+}
+
+function markerPlace(array, map){
+  console.log('array for markers', array);
+  
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item) => {
+    console.log('markerPlace', item);
+    const {coordinates} = item.geocoded_column_1;
+    
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+  })
+}
+
 async function mainEvent() {
+  console.log('localStorage check', localStorage.getItem("storedData"));
   const mainForm = document.querySelector(".main_form");
   const loadDataButton = document.querySelector("#data_load");
   const generateListButton = document.querySelector("#generate");
   const textField = document.querySelector("#resto");
-
+  const clearDataButton = document.querySelector("#data_clear");
   const loadAnimation = document.querySelector("#data_load_animation");
+  
   loadAnimation.style.display = "none";
   generateListButton.classList.add("hidden");
 
+  const carto = initMap();
+
   const storedData = localStorage.getItem("storedData");
-  const parsedData = JSON.parse(storedData);
-  if (parsedData.length > 0) {
+  let parsedData = JSON.parse(storedData);
+  if (parsedData?.length > 0) {
     generateListButton.classList.remove("hidden");
   }
 
   let currentList = [];
+
   loadDataButton.addEventListener("click", async (submitEvent) => {
     console.log("Loading Data");
     loadAnimation.style.display = "inline-block";
@@ -60,8 +92,14 @@ async function mainEvent() {
     const storedList = await results.json();
 
     localStorage.setItem("storedData", JSON.stringify(storedList));
+    parsedData = storedList;
+
+    if(storedList?.length > 0){
+      generateListButton.classList.remove("hidden");
+    }
+
     loadAnimation.style.display = "none";
-    // console.table(storedList);
+    // consoe.tabe(storedist);
   });
 
   generateListButton.addEventListener("click", (event) => {
@@ -69,6 +107,7 @@ async function mainEvent() {
     currentList = cutRestaurantList(parsedData);
     console.log(currentList);
     injectHTML(currentList);
+    markerPlace(currentList, carto);
   });
 
   textField.addEventListener("input", (event) => {
@@ -77,7 +116,14 @@ async function mainEvent() {
 
     console.log(newList);
     injectHTML(newList);
+    markerPlace(newList, carto);
   });
+
+  clearDataButton.addEventListener("click", (event) => {
+    console.log('clear browser data');
+    localStorage.clear();
+    console.log('localStorage check', localStorage.getItem("storedData"));
+  })
 }
 
 document.addEventListener("DOMContentLoaded", async () => mainEvent());
